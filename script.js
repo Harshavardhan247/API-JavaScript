013fba0b8b9bcab1bd5c49e94c65066f5732eb4a
harshavardhan Reddy <o.harshavardhan@gmail.com>
2018-06-30 22:51:08 -0400

    added a json filr and fetched the data onto the view

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.getElementById('getText').addEventListener
 ('click', getText);
 
+document.getElementById('getUsers').addEventListener
+('click', getUsers);
+
 // function getText(){
 //     fetch('sample.txt')
 //     .then(function(res){
@@ -21,5 +24,25 @@ function getText(){
     })
     .catch((err)=>{
         console.log(err);
+    })
+}
+
+function getUsers(){
+    fetch('users.json')
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        let output = '<h2>Users</h2>';
+        data.forEach((user) => {
+            output += `
+            <ul>
+                <li>ID: ${user.id}</li>
+                <li>Name: ${user.name}</li>
+                <li>Email: ${user.email}</li>
+            </ul>
+            `;
+        });
+        document.getElementById('output').innerHTML = output;
     })
 }
